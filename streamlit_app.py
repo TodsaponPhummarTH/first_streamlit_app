ccc5f0e7457b8be4c2254fe5dc2f6b97ed097a54
TodsaponPhummarTH <87286426+TodsaponPhummarTH@users.noreply.github.com>
2023-11-15 14:24:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #pull the data into a dataframe
 import pandas as pd
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #Show the dataframe on webpage
-streamlit.dataframe(my_fruit_list)
+st.dataframe(my_fruit_list)
