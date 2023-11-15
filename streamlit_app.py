e94773fe58b2d654516ca1682434d17b4970d46f
TodsaponPhummarTH <87286426+TodsaponPhummarTH@users.noreply.github.com>
2023-11-15 14:14:50 +0700

    Update streamlit_app.py
    fixed emoji into ''

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.title('My Parent New Healthy Diner')
 
 st.header('Breakfast Menu')
-st.text(🥣'Omega 3 & Blueberry Oatmeal')
-st.text(🥗'Kale, Spinach & Rocket Smoothie')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free Range Egg')
 st.text('🥑🍞 Avocado Taost')
 
