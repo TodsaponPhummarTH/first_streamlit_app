fdaaaec41846742d8ca8c5cc0cfeaa21152c5f70
TodsaponPhummarTH <87286426+TodsaponPhummarTH@users.noreply.github.com>
2023-11-25 13:19:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,13 @@ st.dataframe(fruits_to_show)
 
 #New section to display from API
 st.header("Fruityvice Fruit Advice!")
+
+#Enter Text and use as variable
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
 
 
 # write your own comment -what does the next line do? 
