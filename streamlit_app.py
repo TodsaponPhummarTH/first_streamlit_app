36a127622355e5d14d0f234797bcb47d873ca413
TodsaponPhummarTH <87286426+TodsaponPhummarTH@users.noreply.github.com>
2023-11-25 13:02:34 +0700

    Update streamlit_app.py
    Add API request library

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,12 @@ st.dataframe(fruits_to_show)
 
 
 
+#New section to display from API
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
+
