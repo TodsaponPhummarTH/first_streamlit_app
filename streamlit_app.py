dad15a48c30d2722d0cc10f1c8784eb8b31996d3
TodsaponPhummarTH <87286426+TodsaponPhummarTH@users.noreply.github.com>
2023-11-27 16:58:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 
 import streamlit as st
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 st.title('My Parent New Healthy Diner')
 
 st.header('Breakfast Menu')
@@ -10,7 +15,7 @@ st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #pull the data into a dataframe
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -28,7 +33,7 @@ st.header("Fruityvice Fruit Advice!")
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
 
 
@@ -37,7 +42,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 st.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
+
+#dont run anything past here when troubleshoot
+st.stop()
+
+#import snowflake.connector
 
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
